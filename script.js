136ee610df87353461b0ee719e8624e7936d0bba
Gopakumar s <artist.gopan@gmail.com>
2021-07-25 20:50:04 -0700

    changes in javascript

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function showTime(){
-    var date = new Date(); 
+    var date = new Date();
+    var h =date.getHours();
+    var m=date.getMinutes();
+    var s=date.getSeconds();
+
+    var time= hh + ":" + mm + ":" + ss ;
+
+    document.getElementById("myDigitalClock").innerHTML=time;
+    document.getElementById("myDigitalClock").textContent=time;
 }
