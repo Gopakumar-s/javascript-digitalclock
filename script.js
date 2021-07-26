f9f1b3e0a3a2128ec86a34f380e57bd6367db2d3
Gopakumar s <artist.gopan@gmail.com>
2021-07-25 22:53:04 -0700

    Project finalised

--- script.js
+++ script.js
@@ -3,9 +3,25 @@ function showTime(){
     var h =date.getHours();
     var m=date.getMinutes();
     var s=date.getSeconds();
+    var session = "AM"
 
-    var time= hh + ":" + mm + ":" + ss ;
+    if (h == 0){
+        h = 12;
+    }
+    if (h > 12){
+        h = h - 12;
+        session = "PM"
+    }
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
 
-    document.getElementById("myDigitalClock").innerHTML=time;
+    var time = h + ":" + m + ":" + s + " " + session ;
+
+    document.getElementById("myDigitalClock").innerText=time;
     document.getElementById("myDigitalClock").textContent=time;
+
+    setTimeout(showTime,1000);
 }
+
+showTime();
